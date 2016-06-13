d28537fe599704842f9152bf28c5bdb4ec3717a5
Duong Quang Son <quangson91@gmail.com>
2016-06-13 23:46:53 +0700

    implement item list

--- app/src/main/java/vnzit/com/sampletransitionanimation/Item.java
+++ app/src/main/java/vnzit/com/sampletransitionanimation/Item.java
@@ -1,7 +1,24 @@
 package vnzit.com.sampletransitionanimation;
 
+import java.io.Serializable;
+
 /**
  * Created by sh on 6/13/16.
  */
-public class Item {
+public class Item implements Serializable{
+    private int icon;
+    private String content;
+
+    public Item(int icon, String content) {
+        this.icon = icon;
+        this.content = content;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
--- app/src/main/java/vnzit/com/sampletransitionanimation/ItemAdapter.java
+++ app/src/main/java/vnzit/com/sampletransitionanimation/ItemAdapter.java
@@ -1,7 +1,52 @@
 package vnzit.com.sampletransitionanimation;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
  * Created by sh on 6/13/16.
  */
-public class ItemAdapter {
+public class ItemAdapter extends ArrayAdapter<Item> {
+    private static final int[] IMGS = {R.drawable.android1, R.drawable.android2};
+    public ItemAdapter(Context context, int resource, List<Item> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View v, ViewGroup parent) {
+        final ViewHolder holder;
+        if (v == null) {
+            v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+            holder = new ViewHolder(v);
+            v.setTag(holder);
+        } else {
+            holder = (ViewHolder) v.getTag();
+        }
+        holder.bind(getItem(position), position);
+        return v;
+    }
+
+    private static class ViewHolder {
+
+        ImageView ivIcon;
+        TextView tvContent;
+        ViewHolder(View v) {
+            ivIcon = (ImageView) v.findViewById(R.id.ivIcon);
+            tvContent = (TextView) v.findViewById(R.id.tvContent);
+        }
+
+        @SuppressLint("DefaultLocale")
+        public void bind(Item item, int position) {
+            ivIcon.setImageResource(item.getIcon());
+            tvContent.setText(String.format("Content: %s", item.getContent()));
+        }
+    }
 }
