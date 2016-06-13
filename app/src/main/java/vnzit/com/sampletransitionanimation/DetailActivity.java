35cb023c3c2223f797d3107ca0d13bb345de87a3
Duong Quang Son <quangson91@gmail.com>
2016-06-13 23:47:21 +0700

    implement detail activity

--- app/src/main/java/vnzit/com/sampletransitionanimation/DetailActivity.java
+++ app/src/main/java/vnzit/com/sampletransitionanimation/DetailActivity.java
@@ -1,7 +1,39 @@
 package vnzit.com.sampletransitionanimation;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by sh on 6/13/16.
  */
-public class DetailActivity {
+public class DetailActivity extends AppCompatActivity {
+    public static final String EXTRA_ITEM = "extra_item";
+
+    ImageView ivIcon;
+    TextView tvContent;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        ivIcon = (ImageView) findViewById(R.id.ivIcon);
+        tvContent = (TextView) findViewById(R.id.tvContent);
+
+        onNewIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (intent == null || !intent.hasExtra(EXTRA_ITEM)) {
+            finish();
+            return;
+        }
+        final Item item = (Item) intent.getSerializableExtra(EXTRA_ITEM);
+        ivIcon.setImageResource(item.getIcon());
+        tvContent.setText(item.getContent());
+    }
 }
